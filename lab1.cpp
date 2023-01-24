e34701357a5ecf09ce0b023f48e88db532af3fe9
Ailand <ailandjparriott@gmail.com>
2023-01-24 09:03:31 -0800

    my first commit for lab1!

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+/* Ailand Parriott
+ * lab 1
+ * lab1.cpp
+ * 23.01.24
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;    
+
+    cout << a << endl;
+    cout << "EEP!~\n";
+
+
+    return 0;
+}
+
+
